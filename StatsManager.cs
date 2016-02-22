0ff883f5a95a98a38fb1cbf8dbd56e6fbc6ed4ab
DannyPratt <PrattDJ31@gmail.com>
2016-02-22 09:50:08 -0600

    Added files via upload

---
+++ StatsManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatsManager{
+
+	public static int lives;
+	public static int moves;
+	public static float health;
+	public static string userName;
+	public static float questionIndex;
+	public static float answerIndex;
+}
